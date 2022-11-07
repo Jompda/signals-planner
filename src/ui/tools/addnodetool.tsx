a5be8d598dc1e0541d89dfd1ba38a24ac75bb597
Jompda <45610052+Jompda@users.noreply.github.com>
2022-11-07 19:42:41 +0200

    Added Signals Squad to AddNode icon

--- src/ui/tools/addnodetool.tsx
+++ src/ui/tools/addnodetool.tsx
@@ -1,15 +1,19 @@
 import Tool from '../tool'
+import { Symbol as MilSymbol } from 'milsymbol'
 
 
 class AddNodeTool extends Tool {
+    public symbol: MilSymbol
     constructor() {
+        const symbol = new MilSymbol({ sidc: 'SFGPUUS----B', size: 16 })
         super({
             icon: (
                 <div className='toolbutton-icon' title='Add Node'>
-                    <i className="fa-brands fa-hashnode"></i>
+                    <img src={symbol.toDataURL()} />
                 </div>
             )
         })
+        this.symbol = symbol
     }
     enable() {
 
