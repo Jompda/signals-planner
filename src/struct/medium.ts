81882216d18a0d05c94563a3cf87e17ab3533239
Joni Rapo <jonirapo@gmail.com>
2022-11-12 21:20:21 +0200

    Added base class system for radios and cables

---
+++ src/struct/medium.ts
@@ -0,0 +1,54 @@
+import Link from "./link"
+
+
+export type MediumType = 'radio' | 'cable'
+
+
+export class Medium {
+    public type: MediumType
+    constructor(type: MediumType) {
+        this.type = type
+    }
+}
+
+
+export interface RadioMediumOptions {
+    frequency: number
+    beamWidth?: number
+}
+export class RadioMedium extends Medium {
+    public frequency: number
+    public beamWidth?: number
+    constructor(options: RadioMediumOptions) {
+        super('radio')
+        Object.apply(this, options)
+    }
+    // TODO: Implement a prediction model i.e. Egli.
+    calculate(link: Link) {
+        return {
+            dBm: NaN,
+            RSSI: NaN,
+            CINR: NaN,
+            cost: NaN
+        }
+    }
+}
+
+
+export interface CableMediumOptions {
+    baseCost: number
+    distanceMultiplier: number
+}
+export class CableMedium extends Medium {
+    public baseCost: number
+    public distanceMultiplier: number
+    constructor(options: CableMediumOptions) {
+        super('cable')
+        Object.apply(this, options)
+    }
+    calculate(link: Link) {
+        return {
+            cost: NaN
+        }
+    }
+}
