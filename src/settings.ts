72390ee6ae966585580b5db826177516c07e95dd
Joni Rapo <45610052+Jompda@users.noreply.github.com>
2024-07-28 17:00:25 +0300

    Switched to a better data structure

--- src/settings.ts
+++ src/settings.ts
@@ -1,15 +1,13 @@
 
-const defaultSettings = [
+const defaultSettings = new Map([
     ['defaultEmitterHeight', 'number|25'], // NOTE: Medium-specific emitter heights?
     ['defaultLinkMedium', 'string|SHF1'],
     ['defaultUnitSIDC', 'string|30031000001211000000']
-]
+])
 
 export function getSetting(name: string) {
     let item = localStorage.getItem(name)
-    if (!item)
-        for (const setting of defaultSettings)
-            if (setting[0] === name) item = setting[1]
+    if (!item) item = defaultSettings.get(name)
     const parts = item.split("|", 2)
     if (parts[0] === "number") return parseFloat(parts[1])
     return parts[1]
