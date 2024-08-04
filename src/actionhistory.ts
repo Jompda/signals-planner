7e3693a3583bb7b80c1b2eabd841cc2a7834655d
Joni Rapo <45610052+Jompda@users.noreply.github.com>
2024-08-04 14:43:30 +0300

    Actionhistory now has a max length. Settings now support json

--- src/actionhistory.ts
+++ src/actionhistory.ts
@@ -1,39 +1,43 @@
 import { notifications } from '.'
 import Action from './actions/action'
+import { getSetting } from './settings'
+
+
+// TODO: Make modifiable
+let actionHistoryLength = getSetting('defaultActionHistoryLength') as number
 
 
 export type ActionEvent = 'structureUpdate' | 'drawUpdate' | 'selectionUpdate'
-/**
- * Event is one of ActionEvents
- */
+/** Event is one of ActionEvents */
 export const actionEvents = new EventTarget()
+// temp listeners
 actionEvents.addEventListener('structureUpdate', () => console.log('structureUpdate'))
 actionEvents.addEventListener('drawUpdate', () => console.log('drawUpdate'))
 actionEvents.addEventListener('selectionUpdate', () => console.log('selectionUpdate'))
 
 
 let i = -1
-const timeline = new Array<Action>()
-export function getActionTimeline() {
-    return timeline
+const history = new Array<Action>()
+export function getActionHistory() {
+    return history
 }
 
 
 export function addAction(action: Action) {
     if (action.added) return
     action.added = true
-    if (i < timeline.length - 1)
-        timeline.splice(i + 1, timeline.length)
-    timeline.push(action)
-    i++
+    if (i < history.length - 1) history.splice(i + 1, history.length)
+    if (history.length >= actionHistoryLength) history.shift()
+    else ++i
+    history.push(action)
 }
 
 
 export function undo() {
-    if (i > -1) timeline[i--].reverse()
+    if (i > -1) history[i--].reverse()
     notifications.info('Undo', `History: ${i + 1}`)
 }
 export function redo() {
-    if (i < timeline.length - 1) timeline[++i].forward()
+    if (i < history.length - 1) history[++i].forward()
     notifications.info('Redo', `History: ${i + 1}`)
 }
--- src/settings.ts
+++ src/settings.ts
@@ -3,17 +3,21 @@ import options from '../options'
 const defaultSettings = new Map([
     ['defaultEmitterHeight', 'number|25'],
     ['defaultLinkMedium', 'string|SHF1'],
-    ['defaultUnitSIDC', 'string|30031000001211000000']
+    ['defaultUnitSIDC', 'string|30031000001211000000'],
+    ['defaultActionHistoryLength', 'number|500']
 ])
 for (const field in options)
     defaultSettings.set(field, 'string|' + (options as any)[field])
 
-export function getSetting(name: string) {
+export function getSetting(name: string, jsonReviver?: (this: any, key: string, value: any) => any) {
     let item = localStorage.getItem(name)
     if (!item) item = defaultSettings.get(name)
-    const parts = item.split("|", 2)
-    if (parts[0] === "number") return parseFloat(parts[1])
-    return parts[1]
+    const [type, value] = item.split("|", 2)
+    switch (type) {
+        case 'number': return parseFloat(value)
+        case 'json': return JSON.parse(value, jsonReviver)
+        default: return value
+    }
 }
 
 export function setSetting(name: string, value: any) {
